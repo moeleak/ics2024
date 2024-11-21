3822ebd996297c31fbb98b198e52b06e7837e39e
tracer-ics2024 <tracer@njuics.org>
2024-11-21 11:47:14 +0000

    >  compile NEMU 231220000 张三 Linux lolimaster 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  11:47:14 up 1 day,  2:49,  5 users,  load average: 1.99, 1.74, 1.67

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,6 +24,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  size_t num_regs = 32;
+  for (int i = 0; i < num_regs; i++)
+    printf("%s: %d\n", regs[i], cpu.gpr[i]);
+
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
